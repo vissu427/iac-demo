eddcf80e2c51987a6b4b8a550ded3c142d723a35
vissu427 <63368507+vissu427@users.noreply.github.com>
2026-09-03 08:22:15 +0530

    Rename S3 bucket from awsdevopstraning7898 to Viswa7898

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "awsdevopstraning7898"
+  bucket = "Viswa7898"
   acl    = "private"
 
   control_object_ownership = true
